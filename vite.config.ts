76102c2b73aa1629e1719113e87604a4895d84f4
Ian-potter <Ian@portkey.finance>
2024-10-18 14:40:56 +0800

    ci: 🎡 change config

--- vite.config.ts
+++ vite.config.ts
@@ -6,40 +6,28 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import fs from "fs";
 
 const rewriteEnv = () => {
-  return {
-    name: "rewriteEnv",
-    buildStart() {
-      const name = process.env.ENV_NAME || "testnet";
-      console.log(name, "name===");
-      try {
-        const data = fs.readFileSync(`.env.${name}`, "utf8");
-        console.log(data, "data==");
-        data.split("\n").forEach(function (item) {
-          const [key, value] = item.split("=");
-          if (key) {
-            process.env[key.trim()] = value?.trim();
-          }
-        });
-      } catch (error) {
-        console.error(`Failed to load environment variables from file ${name}`);
+  const name = process.env.ENV_NAME || "testnet";
+  console.log(name, "name===");
+  try {
+    const data = fs.readFileSync(`.env.${name}`, "utf8");
+    console.log(data, "data==");
+    data.split("\n").forEach(function (item) {
+      const [key, value] = item.split("=");
+      if (key) {
+        process.env[key.trim()] = value?.trim();
       }
-    },
-    buildEnd() {
-      const line = "---------------------------------------------------------";
-      const msg = `❤️❤️❤️ Build end game! ❤️❤️❤️`;
-      process.stdout.write(`${line}\n${msg}\n${line}\n`);
-
-      process.stdout.write(`✨ Done ✨\n`);
-    },
-  };
+    });
+  } catch (error) {
+    console.error(`Failed to load environment variables from file ${name}`);
+  }
 };
 
+rewriteEnv();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     nodePolyfills(),
-    rewriteEnv(),
     // mkcert()
   ],
   server: {
